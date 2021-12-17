1623dc978fab6ad79e4d3f5d14207603f1b2300a
Washington Campos <washingtoncampos9@gmail.com>
2021-12-17 14:54:08 -0300

    feat: create test Should render correct active list items filterr

--- src/components/Todo/todo.test.js
+++ src/components/Todo/todo.test.js
@@ -53,6 +53,29 @@ describe('<Todo />', () => {
 		expect(clearCompletedButton).not.toBeInTheDocument();
 	});
 
+	it('Should render correct active list items filter', () => {
+		render(<MockTodoComponent />);
+
+		fireEvent.change(screen.getByPlaceholderText(/Create a new todo/i), {
+			target: { value: 'Wash the dishes' },
+		});
+		fireEvent.click(screen.getByTitle(/Form send icon/i));
+		fireEvent.change(screen.getByPlaceholderText(/Create a new todo/i), {
+			target: { value: 'Finish homework' },
+		});
+		fireEvent.click(screen.getByTitle(/Form send icon/i));
+
+		const filterActiveButton = screen.getByRole('button', { name: /Active/i });
+
+		fireEvent.click(filterActiveButton);
+
+		const items = screen.getAllByRole('listitem');
+		const itemsLeft = screen.getByText(/2 items left/i);
+
+		expect(items.length).toEqual(2);
+		expect(itemsLeft).toBeInTheDocument();
+	});
+
 	it('Should render correct completed list items filter', () => {
 		render(<MockTodoComponent />);
 
