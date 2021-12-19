b0b0cd2106d0820524236c21c112d5365c01fa8b
Washington Campos <washingtoncampos9@gmail.com>
2021-12-19 16:10:00 -0300

    refactor: export all components from a single file

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { Content } from 'components/Content';
+export { Filters } from 'components/Filters';
+export { Footer } from 'components/Footer';
+export { Header } from 'components/Header';
+export { Input } from 'components/Input';
+export { List } from 'components/List';
+export { Main } from 'components/Main';
+export { Todo } from 'components/Todo';
